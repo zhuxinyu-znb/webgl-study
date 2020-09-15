58ea2eca351dfc167a7a7ccf9b539f1532369257
zhuxinyu <zhuxinyu05@meituan.com>
2020-09-15 23:50:28 +0800

    feat: 添加动画

--- examples/webgl/three/demo1/demo.js
+++ examples/webgl/three/demo1/demo.js
@@ -1,6 +1,18 @@
-// 1. 设置场景
+// 增加统计功能
+const initStats = () => {
+  const stats = new Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.getElementById('Stats-output').appendChild(stats.domElement);
+  return stats;
+}
+const stats = initStats();
+
+// 设置场景
 const scene = new THREE.Scene();
-// 2. 设置相机
+// 设置相机
 const camera = new THREE.PerspectiveCamera(
   50,
   window.innerWidth / window.innerHeight,
@@ -12,17 +24,17 @@ camera.position.y = 40;
 camera.position.z = 30;
 camera.lookAt(scene.position);
 
-// 3. 设置绘制对象
+// 设置绘制对象
 const renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(new THREE.Color(0xeeeeee));
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMapEnabled = true;
 
-// 4. 设置辅助测试工具
+// 设置辅助测试工具
 const axes = new THREE.AxesHelper(20);
 scene.add(axes);
 
-// 5. 设置平面
+// 设置平面
 const planeGeometry = new THREE.PlaneGeometry(70, 50, 1, 1);
 const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
@@ -33,7 +45,7 @@ plane.position.z = 0;
 plane.receiveShadow = true;
 scene.add(plane);
 
-// 6. 设置立方体
+// 设置立方体
 const cubeGeometry = new THREE.CubeGeometry(4, 4, 4);
 const cubeMaterial = new THREE.MeshLambertMaterial({
   color: 0xff0000,
@@ -45,7 +57,7 @@ cube.position.z = 0;
 cube.castShadow = true;
 scene.add(cube);
 
-// 7. 设置球体
+// 设置球体
 const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
 const sphereMaterial = new THREE.MeshLambertMaterial({
   color: 0x7777ff,
@@ -57,12 +69,33 @@ sphere.position.z = -2;
 sphere.castShadow = true;
 scene.add(sphere);
 
-// 8. 设置点光源
+// 设置点光源
 const spotLight = new THREE.SpotLight(0xffffff);
 spotLight.position.set(-40, 60, -10);
 spotLight.castShadow = true;
 scene.add(spotLight);
 
-// 9. 将renderer的输出挂载到body上
+let step = 0;
+// 引入动画
+const render = () => {
+  // 更新统计图
+  stats.update();
+
+  // 转动方块
+  cube.rotation.x += 0.02;
+  cube.rotation.y += 0.02;
+  cube.rotation.z += 0.02;
+
+  // 球体跳跃
+  step += 0.04;
+  sphere.position.x = 20 + (10 * Math.cos(step));
+  sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+// 将renderer的输出挂载到body上
 document.body.appendChild(renderer.domElement);
-renderer.render(scene, camera);
+
+
+render()
